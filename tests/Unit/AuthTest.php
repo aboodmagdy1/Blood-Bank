07e8dff0ac9e5b4c541b1c0286c29c938ca6339f
Abood Magdy <c.aboodmagdy@gmail.com>
2025-05-12 06:48:31 +0300

    test:register endpoint

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -20,7 +20,7 @@ class AuthTest extends TestCase
     protected $bloodType;
     protected $city;
     protected $client;
-
+    protected $governorate;
     protected function setUp(): void
     {
         parent::setUp();
@@ -34,6 +34,8 @@ class AuthTest extends TestCase
                 'governorate_id' => $governorate->id
             ]);
     
+            $this->governorate = Governorate::create(['name' => 'Cairo']);
+
             // Create a client and generate token
             $this->client = Client::create([
                 'name' => 'Test User',
@@ -93,5 +95,75 @@ class AuthTest extends TestCase
        
     }
 
+        /** @test */
 
+    public function it_can_get_profile()
+    {
+        $response = $this->getJson('/api/v1/profile',[
+            'Authorization' => 'Bearer ' . $this->client->api_token
+        ]);
+
+        $response->assertJsonStructure([
+            'status',
+            'message',
+            'data' => [
+                 [
+                    'name',
+                    'email',
+                    'phone',
+                    'blood_type_id',
+                    'city_id',
+                    'd_o_b',
+
+                ]
+            ]
+        ])->assertJson([
+            'status' => 1,
+            'message' => 'User data',
+            'data' => [
+               [ 'name' => $this->client->name,
+                'email' => $this->client->email,
+                'phone' => $this->client->phone,
+                'blood_type_id' => $this->client->blood_type_id,
+                'city_id' => $this->client->city_id,
+                'd_o_b' => $this->client->d_o_b,
+                ]
+            ]
+        ]);
+        
+        
+        
+        
+    }
+
+    /** @test */
+    public function it_can_register()
+    {
+        $response = $this->postJson('/api/v1/register',[
+            'name' => 'Test User',
+            'email' => 'test-1@example.com',
+            'phone' => '1234567880',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'd_o_b' => '1990-01-01',
+            'city_id' => $this->city->id,
+            'governorate_id' => $this->governorate->id,
+            'blood_type_id' => $this->bloodType->id,
+            'last_donation_date' => '2023-01-01',
+
+        ]);
+
+        $response->assertJson([
+            'status' => 1,
+            'message' => 'client created successfuly',
+            'data' => [
+                'client' => [
+                    'name' => 'Test User',
+                    'email' => 'test-1@example.com',
+                    'phone' => '1234567880',
+                    'blood_type_id' => $this->bloodType->id,
+                ]
+            ]   
+        ]);
+    }
 } 
